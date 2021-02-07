0ef860514d15ba79f75c4a8eb1ece24a21a05d40
adilku <adhan2a@gmail.com>
2021-02-07 14:36:56 +0300

    bugfixes

--- Build/Debug/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documents/PathPlanningProject2d/Src
-# Build directory: D:/Documents/PathPlanningProject2d/Build/Debug/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
